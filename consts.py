97f0af486255a70d3c29f0a4b14fa4acae2a43eb
LennartH21 <lennart.hubbers@gmail.com>
2024-09-10 17:16:37 +0200

    write a title for it

--- consts.py
+++ consts.py
@@ -1,9 +1,9 @@
 inputs = 91
-clmax_landing = 2.1
+clmax_landing = 2.6
 approach_speed = 78
 landing_massfraction = 0.79
 landing_temp_diff = 15
-landing_fieldlengthreq = 1786
+landing_fieldlengthreq = 1981.2
 cruise_massfraction = 0.95
 cruise_altitude = 9449.8
 cruise_minmach = 0.85
@@ -12,20 +12,21 @@ aspect_ratio = 10
 
 to_massfraction = 0.85
 to_altitude = 7400
-to_cL = 0.633 #to be changed
+to_cL = 2.036181377 #to be changed
 to_field_length = 3048
 to_oswald_efficiency = 0.8286
-to_temp = 288.15-0.0065*(to_altitude)
+to_temp = 298
+to_pressure = 95457.84253
 
 bypass_ratio = 10
 wetted_ratio = 6
-friction_coefficient = 0.0027
+friction_coefficient = 0.0028
 ref_altitude = 100
 cruise_temp = 288.15-0.0065*(cruise_altitude-ref_altitude)
 parasite_drag = 0.0075
 span_efficiency = 0.97
 
 climbgradient_massfraction = [1, 1, 1, 1, 0.79]
-climbgradient_zerodrag = [0.0162, 0.0362, 0.0357, 0.0557, 0.0552, 0.0752] # taken from drag polar D:38-
-climbgradient_gradient = [3.2, 0 , 2.4, 1.2, 2.1]
+climbgradient_zerodrag = [0.0758, 0.0563, 0.0363, 0.0168, 0.0558] # taken from drag polar D:38-
+climbgradient_gradient = [3.2, 0 , 0, 1.2, 2.1]
 climbgradient_oswaldfactor = [0.867622569, 0.828622569, 0.828622569, 0.789622569, 0.867622569]
