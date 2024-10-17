012d53998633a3907f96a790e7e659773af974a5
CShiyyy <136488761+CShiyyy@users.noreply.github.com>
2024-10-17 17:02:05 +0200

    Class_II_weight estimation and unit conversion code

--- unit_conversion.py
+++ unit_conversion.py
@@ -9,6 +9,7 @@ Created on Thu Oct 17 14:00:36 2024
 def convert_units(value, unit, to_metric=True):
     metric_to_imperial = {
         'cm': value / 2.54,
+        'meters_inches': value / 0.0254,  # meters to inches
         'm': value / 0.3048,
         'km': value / 1.60934,
         'g': value / 0.453592,
@@ -21,11 +22,13 @@ def convert_units(value, unit, to_metric=True):
         'm^3': value / 0.0283168,
         'newtons': value / 4.44822,  # N to lbf
         'pascals': value / 47.8803,  # Pa to psf
+        'pascals_psi': value / 6894.76,  # Pa to psi
         'newton_meters': value / 1.35582,  # Nm to lb-ft
     }
 
     imperial_to_metric = {
         'in': value * 2.54,
+        'inches_meters': value * 0.0254,  # inches to meters
         'ft': value * 0.3048,
         'yds': value * 0.9144,
         'miles': value * 1.60934,
@@ -40,6 +43,7 @@ def convert_units(value, unit, to_metric=True):
         'ft^3': value * 0.0283168,
         'pounds_force': value * 4.44822,  # lbf to N
         'psf': value * 47.8803,  # psf to Pa
+        'psi': value * 6894.76,  # psi to Pa
         'pound_feet': value * 1.35582,  # lb-ft to Nm
     }
 
@@ -48,5 +52,5 @@ def convert_units(value, unit, to_metric=True):
     else:
         return metric_to_imperial.get(unit, "Unknown unit")
 
-print(convert_units(1000, 'ft'))  # Converts 10 feet to meters (default to_metric=True)
+print(convert_units(4,'m^2', False))  # Converts 10 feet to meters (default to_metric=True)
 
